a181b7cbc125f6fd39479123a8705465451f28da
Jakub Englický <englicky@mf.cz>
2017-08-17 14:32:30 +0200

    add comments

--- src/FacebookSocialPluginsControl.php
+++ src/FacebookSocialPluginsControl.php
@@ -11,4 +11,13 @@ class FacebookSocialPluginsControl extends Nette\Application\UI\Control
         $this->template->appId = $appId;
         $this->template->render(__DIR__ . '/templates/fbRoot.latte');
     }
+
+    public function renderComments($link, $limit = 5, $width = 550)
+    {
+        $this->template->link = $link;
+        $this->template->limit = $limit;
+        $this->template->width = $width;
+
+        $this->template->render(__DIR__ . '/templates/fbComments.latte');
+    }
 }
