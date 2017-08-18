069860e68e3d6e3c57ccb56980c811dce3d0c8a7
Jakub Englický <englicky@mf.cz>
2017-08-18 12:57:44 +0200

    add throw exceptions into all methods

--- src/SocialPlugins/Facebook/Facebook.php
+++ src/SocialPlugins/Facebook/Facebook.php
@@ -2,7 +2,7 @@
 
 namespace SocialPlugins;
 
-use SocialPlugins\Facebook\InputException;
+use SocialPlugins\Facebook\Exception\InputException;
 
 class Facebook
 {
@@ -31,18 +31,31 @@ class Facebook
     const LAYOUT_BUTTON = 'button';
     const LAYOUT_STANDARD = 'standard';
 
+    const LIKE_FOLLOW_LAYOUT_OPTIONS = [self::LAYOUT_BUTTON_COUNT, self::LAYOUT_BUTTON, self::LAYOUT_BOX_COUNT, self::LAYOUT_STANDARD];
+    const SHARE_LAYOUT_OPTIONS = [self::LAYOUT_BUTTON_COUNT, self::LAYOUT_BUTTON, self::LAYOUT_BOX_COUNT];
+
+
     /* Sizes */
     const SIZE_SMALL = 'small';
     const SIZE_LARGE = 'large';
+
+    const SIZE_OPTIONS = [self::SIZE_SMALL, self::SIZE_LARGE];
 
     /* Page Tabs */
     const PAGE_TIMELINE = 'timeline';
     const PAGE_EVENTS = 'events';
     const PAGE_MESSAGES = 'messages';
 
+    const TABS_OPTIONS = [self::PAGE_TIMELINE, self::PAGE_EVENTS, self::PAGE_MESSAGES];
+
     public function __construct($appId)
     {
         $this->latte = new \Latte\Engine();
+
+        if (is_int($appId) || $appId == 0 || $appId == NULL) {
+            throw new InputException('Facebook AppID must be defined in correct format.',500);
+        }
+
         $this->appId = $appId;
 
         $this->setLocale('cs_CZ');
@@ -70,6 +83,14 @@ class Facebook
 
     public function renderComments($link, $limit = 5, $width = 550)
     {
+        if ($link === '' || $link == NULL  || $link === 'NULL') {
+            throw new InputException('Link must be defined.', 500);
+        }
+
+        if (!is_integer($limit) || !is_integer($width)) {
+            throw new InputException('These values (limit, width) must be integer.', 500);
+        }
+
         $parameters = array(
             "link" => $link,
             "limit" => $limit,
@@ -81,20 +102,36 @@ class Facebook
 
     /**
      * @param string $link
-     * @param boolean $shareButton|false
+     * @param boolean $shareButton|FALSE
      * @param string $layout|button_count
      * @param string $size|small
-     * @param string $faces|false
+     * @param string $faces|FALSE
      * return string of html
      */
-    public function renderLikeButton($link, $shareButton = false, $layout = self::LAYOUT_BUTTON_COUNT, $size = self::SIZE_SMALL, $faces = false)
+    public function renderLikeButton($link, $shareButton = FALSE, $layout = self::LAYOUT_BUTTON_COUNT, $size = self::SIZE_SMALL, $showFaces = FALSE)
     {
+        if ($link === '' || $link == NULL  || $link === 'NULL') {
+            throw new InputException('Link must be defined.', 500);
+        }
+
+        if (!is_bool($shareButton) || !is_bool($showFaces)) {
+            throw new InputException('These values (shareButton, showFaces) must be boolean.', 500);
+        }
+
+        if (!in_array($layout,self::LIKE_FOLLOW_LAYOUT_OPTIONS)) {
+            throw new InputException('Layout must be select from options.', 500);
+        }
+
+        if (!in_array($size,self::SIZE_OPTIONS)) {
+            throw new InputException('Size must be select from options.', 500);
+        }
+
         $parameters = array(
             "link" => $link,
             "layout" => $layout,
             "size" => $size,
             "share" => $shareButton,
-            "faces" => $faces,
+            "faces" => $showFaces,
         );
 
         $this->latte->render(__DIR__ . '/templates/fbLike.latte', $parameters);
@@ -104,13 +141,29 @@ class Facebook
      * @param string $link
      * @param string $layout|button_count
      * @param string $size|small
-     * @param boolean $mobileFrame|false
+     * @param boolean $mobileFrame|FALSE
      * return string of html
      */
-    public function renderShareButton($link,$layout = self::LAYOUT_BUTTON_COUNT, $size = self::SIZE_SMALL,$mobileFrame = false)
+    public function renderShareButton($shareLink,$layout = self::LAYOUT_BUTTON_COUNT, $size = self::SIZE_SMALL,$mobileFrame = FALSE)
     {
+        if ($shareLink === '' || $shareLink == NULL  || $shareLink === 'NULL') {
+            throw new InputException('Share link must be defined in correct format.', 500);
+        }
+
+        if (!in_array($layout,self::SHARE_LAYOUT_OPTIONS)) {
+            throw new InputException('Layout must be select from options.', 500);
+        }
+
+        if (!in_array($size,self::SIZE_OPTIONS)) {
+            throw new InputException('Size must be select from options.', 500);
+        }
+
+        if (!is_bool($mobileFrame)) {
+            throw new InputException('This value (mobileFrame) must be boolean.', 500);
+        }
+
         $parameters = array(
-            "link" => $link,
+            "link" => $shareLink,
             "layout" => $layout,
             "size" => $size,
             "mobileFrame" => $mobileFrame,
@@ -127,13 +180,33 @@ class Facebook
      * @param boolean $faces
      * return string of html
      */
-    public function renderFollowButton($link, $width = 200, $size = self::SIZE_SMALL,$layout = self::LAYOUT_BUTTON_COUNT, $faces = false)
+    public function renderFollowButton($fbFollowLink, $width = 200, $size = self::SIZE_SMALL,$layout = self::LAYOUT_BUTTON_COUNT, $showFaces = FALSE)
     {
+        if ($fbFollowLink === '' || $fbFollowLink == NULL || $fbFollowLink === 'NULL') {
+            throw new InputException('Follow link must be defined in correct format.', 500);
+        }
+
+        if (!is_integer($width)) {
+            throw new InputException('Width must be integer.', 500);
+        }
+
+        if (!in_array($size,self::SIZE_OPTIONS)) {
+            throw new InputException('Size must be select from options.', 500);
+        }
+
+        if (!in_array($layout,self::LIKE_FOLLOW_LAYOUT_OPTIONS)) {
+            throw new InputException('Layout must be select from options.', 500);
+        }
+
+        if (!is_bool($showFaces)) {
+            throw new InputException('This value (showFaces) must be boolean.', 500);
+        }
+
         $parameters = array(
-            "link" => $link,
+            "link" => $fbFollowLink,
             "layout" => $layout,
             "size" => $size,
-            "faces" => $faces,
+            "faces" => $showFaces,
             "width" => $width,
         );
 
@@ -145,23 +218,31 @@ class Facebook
      * @param string $tabs
      * @param integer $width|350
      * @param integer $height|500
-     * @param boolean $smallHeader|false
-     * @param boolean $hideCoverPhoto|false
-     * @param boolean $showFaces|false
+     * @param boolean $smallHeader|FALSE
+     * @param boolean $hideCoverPhoto|FALSE
+     * @param boolean $showFaces|FALSE
      * return string of html
      */
-    public function renderPagePlugin($fbPageLink, $tabs = self::PAGE_TIMELINE, $width = 350, $height = 500, $smallHeader = false, $hideCoverPhoto = false, $showFaces = false)
+    public function renderPagePlugin($fbPageLink, $tabs = self::PAGE_TIMELINE, $width = 350, $height = 500, $smallHeader = FALSE, $hideCoverPhoto = FALSE, $showFaces = FALSE)
     {
-        if ($fbPageLink == '' || $fbPageLink == NULL) {
-            throw new \SocialPlugins\Facebook\Exception\InputException('Facebook page URL must be defined.',500);
+        if ($fbPageLink === '' || $fbPageLink == NULL || $fbPageLink === 'NULL') {
+            throw new InputException('Facebook page URL must be defined.',500);
         }
 
         if ($width < 180 || $width > 500) {
-            throw new \SocialPlugins\Facebook\Exception\InputException('Width must be in this range -> (180-500)px.',500);
+            throw new InputException('Width must be in this range -> (180-500)px.',500);
         }
 
         if ($height < 70) {
-            throw new \SocialPlugins\Facebook\Exception\InputException('Height must be bigger then 70px.',500);
+            throw new InputException('Height must be bigger then 70px.',500);
+        }
+
+        if(!in_array($tabs,self::TABS_OPTIONS)) {
+            throw new InputException('Tab must be select from options.', 500);
+        }
+
+        if(!is_bool($smallHeader) || !is_bool($hideCoverPhoto) || !is_bool($showFaces)) {
+            throw new InputException('These values (smallHeader,hideCoverPhoto,showFaces) must be boolean.', 500);
         }
 
         $parameters = array(
